49c922fa0aa8f9b16ed37691d92eaec485e4a7ad
estockwellalpert <erica.stockwell-alpert@velir.com>
2025-07-30 16:42:05 -0400

    author links

--- contentexporttool/src/components/AuthorInfo.tsx
+++ contentexporttool/src/components/AuthorInfo.tsx
@@ -2,6 +2,9 @@
 export const AuthorInfo = () => {
 
   return (
-    <p className="px-4 author-ribbon flex"><img src="https://www.velir.com/-/media/logos/2025/velir-logo--large.svg" /><span>Created by <a href="https://ericastockwellalpert.wordpress.com/">Erica Stockwell-Alpert</a></span></p>
+    <p className="px-4 author-ribbon flex">
+      <a target="_blank" href="https://www.velir.com/"><img src="https://www.velir.com/-/media/logos/2025/velir-logo--large.svg" /></a>
+      <span>Created by <a target="_blank" href="https://ericastockwellalpert.wordpress.com/">Erica Stockwell-Alpert</a></span>
+    </p >
   );
 };
